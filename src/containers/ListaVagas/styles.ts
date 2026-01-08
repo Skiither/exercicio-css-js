d3ed15560c712b75432d92e1a4d630e45008be99
Fernando Neves <fernandodn3006@gmail.com>
2026-01-08 16:38:39 +0000

    finalizando o projeto

--- src/containers/ListaVagas/styles.ts
+++ src/containers/ListaVagas/styles.ts
@@ -6,11 +6,10 @@ const Vacancies = styled.ul`
   column-gap: 16px;
   row-gap: 16px;
   margin-top: 32px;
-}
 
-@media (max-width: 768px) {
-  .vagas {
+  @media (max-width: 768px) {
     grid-template-columns: 1fr;
   }
 `
+
 export default Vacancies
